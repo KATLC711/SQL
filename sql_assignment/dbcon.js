8e88a9b3859dd69d96d078e61c7eeebefb782a3f
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-19 22:32:50 -0500

    update

--- sql_assignment/dbcon.js
+++ sql_assignment/dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'mysql.eecs.oregonstate.edu',
-  user            : 'cheungke',
-  password        : '4019',
-  database        : 'cheungke'
+  connectionLimit: 10,
+  host: 'mysql.eecs.oregonstate.edu',
+  user: 'cs290_cheungke',
+  password: '4019',
+  database: 'cs290_cheungke'
 });
 
 module.exports.pool = pool;
