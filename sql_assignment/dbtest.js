8fa8c84fdef188126524dc35aea97af354329bcb
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-19 22:59:28 -0500

    Update dbtest.js

--- sql_assignment/dbtest.js
+++ sql_assignment/dbtest.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res, next) {
 
 app.get('/insert', function (req, res, next) {
   var context = {};
-  mysql.pool.query("INSERT INTO todo (`name`,`reps`,`weight`,`date`,`unit`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.unit], function (err, result) {
+  mysql.pool.query("INSERT INTO exercise (`name`,`reps`,`weight`,`date`,`unit`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.unit], function (err, result) {
     if (err) {
       next(err);
       return;
