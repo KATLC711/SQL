dd09dc1e8cc6ce71f90114e24a942edde634ebc0
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-20 01:11:45 -0500

    Update dbtest.js

--- sql_assignment/dbtest.js
+++ sql_assignment/dbtest.js
@@ -145,5 +145,7 @@ function getFormattedDate(date_unformmated) {
   var day = date.getDate().toString();
   day = day.length > 1 ? day : '0' + day;
 
-  return month + '/' + day + '/' + year;
+  final = month + '/' + day + '/' + year
+
+  return final.toString();
 }
