9aa92daa89f56f1495fe13497c95978e62cd5854
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-20 00:55:03 -0500

    Update dbtest.js

--- sql_assignment/dbtest.js
+++ sql_assignment/dbtest.js
@@ -94,7 +94,7 @@ app.get('/edit', function (req, res, next) {
     }
     if (result.length == 1) {
       var curVals = result[0];
-      console.log(typof(req.query.date))
+      console.log(typeof (req.query.date))
       mysql.pool.query("UPDATE exercise SET name=?,reps=?,weight=?,date=?,unit=? WHERE id=? ",
         [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.unit || curVals.unit, req.query.id],
         function (err, result) {
